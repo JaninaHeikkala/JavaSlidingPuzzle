a50ea70054173cdb0e090e74f60436b01d0f604f
JaninaHeikkala <janina.heikkala@hotmail.com>
2023-10-22 21:51:41 +0300

    sliding puzzle done, pictures not yet added

--- JavaSlidingPuzzle/src/GameFrame.java
+++ JavaSlidingPuzzle/src/GameFrame.java
@@ -24,39 +24,4 @@ public class GameFrame extends JFrame {
         this.setLocationRelativeTo(null);
     }
 
-    /*private JPanel createButtonPanel() {
-        JPanel panel = new JPanel();
-
-        JLabel difficultyLabel = new JLabel("Difficulty");
-        difficultyLabel.setFont(new Font("Ink Free", Font.BOLD, 75));
-        difficultyLabel.setForeground(new Color(30, 200, 100));
-        panel.add(difficultyLabel);
-
-        // Create buttons for difficulty levels
-        JButton buttonEasy = new JButton("Easy");
-        JButton buttonMedium = new JButton("Medium");
-        JButton buttonHard = new JButton("Hard");
-
-        // Add action listeners to the buttons
-        buttonEasy.addActionListener(e -> setDifficultyAndStart(1));
-        buttonMedium.addActionListener(e -> setDifficultyAndStart(2));
-        buttonHard.addActionListener(e -> setDifficultyAndStart(3));
-
-        // Add buttons to the panel
-        panel.add(buttonEasy);
-        panel.add(buttonMedium);
-        panel.add(buttonHard);
-
-        return panel;
-    }
-
-    private void setDifficultyAndStart(int difficulty) {
-        gamePanel.setDifficulty(difficulty);
-        gamePanel.startGame();
-    }
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new GameFrame());
-    }*/
-
 }
--- JavaSlidingPuzzle/src/GamePanel.java
+++ JavaSlidingPuzzle/src/GamePanel.java
@@ -3,6 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
@@ -17,13 +18,16 @@ public class GamePanel extends JPanel implements ActionListener {
     int gameWidth;
     int gameHeight;
     boolean puzzleCreated = false;
+    boolean buttonsInitialized = false;
+    boolean winScreenCreated = false;
+    boolean restarted = false;
     Timer timer;
     Random random;
     JButton buttonEasy;
     JButton buttonMedium;
     JButton buttonHard;
-    //JButton b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16, b17, b18, b19, b20, b21, b22, b23, b24, b25;
     JButton[][] buttons;
+    JButton restartButton;
 
     GamePanel(){
         random = new Random();
@@ -32,7 +36,7 @@ public class GamePanel extends JPanel implements ActionListener {
         this.setFocusable(true);
         this.setLayout(null);
         startGame();
-        initializeButtons();
+
     }
 
     public void startGame() {
@@ -50,12 +54,23 @@ public class GamePanel extends JPanel implements ActionListener {
 
         //if before the game
         if ( stage == 1 ) {
+            if (restarted) {
+                removeRestartButton();
+            }
+            initializeButtons();
             preGame(g);
+            buttonsInitialized = true;
         }
         if ( stage == 2 ) {
-            removeButtons();
+            removeDifficultyButtons();
             createPuzzle();
             puzzleCreated = true;
+        }
+        if ( stage == 3 ) {
+            removeGameButtons();
+            gameWin(g);
+            winScreenCreated = true;
+            restarted = true;
         }
 
     }
@@ -72,42 +87,46 @@ public class GamePanel extends JPanel implements ActionListener {
 
     public void initializeButtons() {
 
-        buttonEasy = new JButton("easy");
-        buttonEasy.setBounds(SCREEN_WIDTH/2 - (BUTTON_WIDTH/2) - SCREEN_WIDTH/5, (SCREEN_HEIGHT/4)*2, BUTTON_WIDTH, BUTTON_HEIGHT);
-        buttonEasy.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                difficulty = 1;
-                stage = 2;
-                return;
-            }
-        });
+        if (!buttonsInitialized) {
+            buttonEasy = new JButton("easy");
+            buttonEasy.setBounds(SCREEN_WIDTH/2 - (BUTTON_WIDTH/2) - SCREEN_WIDTH/5, (SCREEN_HEIGHT/4)*2, BUTTON_WIDTH, BUTTON_HEIGHT);
+            buttonEasy.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    difficulty = 1;
+                    stage = 2;
+                    return;
+                }
+            });
 
-        buttonMedium = new JButton("medium");
-        buttonMedium.setBounds(SCREEN_WIDTH/2 - (BUTTON_WIDTH/2), (SCREEN_HEIGHT/4)*2, BUTTON_WIDTH, BUTTON_HEIGHT);
-        buttonMedium.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                difficulty = 2;
-                stage = 2;
-                return;
-            }
-        });
+            buttonMedium = new JButton("medium");
+            buttonMedium.setBounds(SCREEN_WIDTH/2 - (BUTTON_WIDTH/2), (SCREEN_HEIGHT/4)*2, BUTTON_WIDTH, BUTTON_HEIGHT);
+            buttonMedium.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    difficulty = 2;
+                    stage = 2;
+                    return;
+                }
+            });
 
-        buttonHard = new JButton("hard");
-        buttonHard.setBounds(SCREEN_WIDTH/2 - (BUTTON_WIDTH/2) + SCREEN_WIDTH/5, (SCREEN_HEIGHT/4)*2, BUTTON_WIDTH, BUTTON_HEIGHT);
-        buttonHard.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                difficulty = 3;
-                stage = 2;
-                return;
-            }
-        });
+            buttonHard = new JButton("hard");
+            buttonHard.setBounds(SCREEN_WIDTH/2 - (BUTTON_WIDTH/2) + SCREEN_WIDTH/5, (SCREEN_HEIGHT/4)*2, BUTTON_WIDTH, BUTTON_HEIGHT);
+            buttonHard.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    difficulty = 3;
+                    stage = 2;
+                    return;
+                }
+            });
 
-        this.add(buttonEasy);
-        this.add(buttonMedium);
-        this.add(buttonHard);
+            this.add(buttonEasy);
+            this.add(buttonMedium);
+            this.add(buttonHard);
+        }
+
+
     }
 
     public void createPuzzle() {
@@ -127,39 +146,278 @@ public class GamePanel extends JPanel implements ActionListener {
 
             buttons = new JButton[gameWidth][gameHeight];
             this.setLayout(new GridLayout(gameWidth, gameHeight));
-            int maxNumber = gameWidth*gameHeight;
+            int maxNumber = gameWidth*gameHeight - 1;
             ArrayList<Integer> numbersAdded = new ArrayList<Integer>(maxNumber);
-            for (int i = 0; i < gameWidth; i++) {
-                for (int j = 0; j < gameHeight; j++) {
+            for (int x = 0; x < gameWidth; x++) {
+                for (int y = 0; y < gameHeight; y++) {
                     int numberToBeAdded = random.nextInt(maxNumber) + 1;
-                    while (numbersAdded.contains(numberToBeAdded)) {
-                        numberToBeAdded = random.nextInt(maxNumber) + 1;
-                        //System.out.println(numberToBeAdded);
+                    if ( numbersAdded.size() != maxNumber) {
+                        while (numbersAdded.contains(numberToBeAdded)) {
+                            numberToBeAdded = random.nextInt(maxNumber) + 1;
+                        }
+                        numbersAdded.add(numberToBeAdded);
+                        buttons[x][y] = new JButton(String.valueOf(numberToBeAdded));
+                        int finalX = x;
+                        int finalY = y;
+                        buttons[x][y].addActionListener(new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                checkButtons(finalX, finalY);
+                                boolean won = checkIfWon();
+                                if (won) {
+                                    stage = 3;
+                                }
+                            }
+                        });
+                        this.add(buttons[x][y], gameWidth * x + y);
                     }
-                    numbersAdded.add(numberToBeAdded);
-                    buttons[i][j] = new JButton(String.valueOf(numberToBeAdded));
-                    this.add(buttons[i][j], gameWidth * i + j);
+                    else {
+                        buttons[x][y] = new JButton("");
+                        int finalX1 = x;
+                        int finalY1 = y;
+                        buttons[x][y].addActionListener(new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                checkButtons(finalX1, finalY1);
+                                boolean won = checkIfWon();
+                                if (won) {
+                                    stage = 3;
+                                }
+                            }
+                        });
+                        this.add(buttons[x][y], gameWidth * x + y);
+                    }
+
                 }
             }
+
         }
-
-
     }
 
     public void setDifficulty(int difficulty) {
         this.difficulty = difficulty;
     }
 
-
-    public void removeButtons() {
+    public void removeDifficultyButtons() {
         this.remove(buttonEasy);
         this.remove(buttonMedium);
         this.remove(buttonHard);
-        this.revalidate(); // Update the panel
-        this.repaint(); // Repaint the panel without the buttons
+        this.revalidate();
+        this.repaint();
     }
+
+    public void checkButtons(int finalX, int finalY) {
+
+        if (!Objects.equals(buttons[finalX][finalY].getText(), "")) {
+
+            JButton clickedButton = buttons[finalX][finalY];
+
+            // clickedButton is in the top left corner
+            if (finalX == 0 && finalY == 0) {
+                // button to the right of clickedButton
+                if (Objects.equals(buttons[finalX + 1][finalY].getText(), "")) {
+                    buttons[finalX + 1][finalY].setText(clickedButton.getText());
+                    buttons[finalX][finalY].setText("");
+                }
+                // button below clickedButton
+                if (Objects.equals(buttons[finalX][finalY + 1].getText(), "")) {
+                    buttons[finalX][finalY + 1].setText(clickedButton.getText());
+                    buttons[finalX][finalY].setText("");
+                }
+            }
+            // clickedButton is in the top right corner
+            else if (finalX == gameWidth - 1 && finalY == 0) {
+                // button to the left of clickedButton
+                if (Objects.equals(buttons[finalX - 1][finalY].getText(), "")) {
+                    buttons[finalX - 1][finalY].setText(clickedButton.getText());
+                    buttons[finalX][finalY].setText("");
+                }
+                // button below clickedButton
+                if (Objects.equals(buttons[finalX][finalY + 1].getText(), "")) {
+                    buttons[finalX][finalY + 1].setText(clickedButton.getText());
+                    buttons[finalX][finalY].setText("");
+                }
+            }
+            // clickedButton is in the bottom left corner
+            else if (finalX == 0 && finalY == gameHeight - 1) {
+                // button to the right of clickedButton
+                if (Objects.equals(buttons[finalX + 1][finalY].getText(), "")) {
+                    buttons[finalX + 1][finalY].setText(clickedButton.getText());
+                    buttons[finalX][finalY].setText("");
+                }
+                // button above clickedButton
+                if (Objects.equals(buttons[finalX][finalY - 1].getText(), "")) {
+                    buttons[finalX][finalY - 1].setText(clickedButton.getText());
+                    buttons[finalX][finalY].setText("");
+                }
+            }
+            // clickedButton is in the bottom right corner
+            else if (finalX == gameWidth-1 && finalY == gameHeight-1) {
+                // button to the left of clickedButton
+                if (Objects.equals(buttons[finalX-1][finalY].getText(), "")) {
+                    buttons[finalX-1][finalY].setText(clickedButton.getText());
+                    buttons[finalX][finalY].setText("");
+                }
+                // button above clickedButton
+                if (Objects.equals(buttons[finalX][finalY - 1].getText(), "")) {
+                    buttons[finalX][finalY - 1].setText(clickedButton.getText());
+                    buttons[finalX][finalY].setText("");
+                }
+            }
+            // clickedButton is on the top row but not in the corners
+            else if (finalY == 0) {
+                // button to the left of clickedButton
+                if (Objects.equals(buttons[finalX-1][finalY].getText(), "")) {
+                    buttons[finalX-1][finalY].setText(clickedButton.getText());
+                    buttons[finalX][finalY].setText("");
+                }
+                // button to the right of clickedButton
+                if (Objects.equals(buttons[finalX + 1][finalY].getText(), "")) {
+                    buttons[finalX + 1][finalY].setText(clickedButton.getText());
+                    buttons[finalX][finalY].setText("");
+                }
+                // button below clickedButton
+                if (Objects.equals(buttons[finalX][finalY + 1].getText(), "")) {
+                    buttons[finalX][finalY + 1].setText(clickedButton.getText());
+                    buttons[finalX][finalY].setText("");
+                }
+            }
+            // clickedButton is on the left column but not in the corners
+            else if (finalX == 0) {
+                // button above clickedButton
+                if (Objects.equals(buttons[finalX][finalY - 1].getText(), "")) {
+                    buttons[finalX][finalY - 1].setText(clickedButton.getText());
+                    buttons[finalX][finalY].setText("");
+                }
+                // button to the right of clickedButton
+                if (Objects.equals(buttons[finalX + 1][finalY].getText(), "")) {
+                    buttons[finalX + 1][finalY].setText(clickedButton.getText());
+                    buttons[finalX][finalY].setText("");
+                }
+                // button below clickedButton
+                if (Objects.equals(buttons[finalX][finalY + 1].getText(), "")) {
+                    buttons[finalX][finalY + 1].setText(clickedButton.getText());
+                    buttons[finalX][finalY].setText("");
+                }
+            }
+            // clickedButton is on the bottom row but not in the corners
+            else if (finalY == gameHeight-1) {
+                // button above clickedButton
+                if (Objects.equals(buttons[finalX][finalY - 1].getText(), "")) {
+                    buttons[finalX][finalY - 1].setText(clickedButton.getText());
+                    buttons[finalX][finalY].setText("");
+                }
+                // button to the right of clickedButton
+                if (Objects.equals(buttons[finalX + 1][finalY].getText(), "")) {
+                    buttons[finalX + 1][finalY].setText(clickedButton.getText());
+                    buttons[finalX][finalY].setText("");
+                }
+                // button to the left of clickedButton
+                if (Objects.equals(buttons[finalX-1][finalY].getText(), "")) {
+                    buttons[finalX-1][finalY].setText(clickedButton.getText());
+                    buttons[finalX][finalY].setText("");
+                }
+            }
+            // clickedButton is on the right column but not in the corners
+            else if (finalX == gameHeight-1) {
+                // button above clickedButton
+                if (Objects.equals(buttons[finalX][finalY - 1].getText(), "")) {
+                    buttons[finalX][finalY - 1].setText(clickedButton.getText());
+                    buttons[finalX][finalY].setText("");
+                }
+                // button below clickedButton
+                if (Objects.equals(buttons[finalX][finalY + 1].getText(), "")) {
+                    buttons[finalX][finalY + 1].setText(clickedButton.getText());
+                    buttons[finalX][finalY].setText("");
+                }
+                // button to the left of clickedButton
+                if (Objects.equals(buttons[finalX-1][finalY].getText(), "")) {
+                    buttons[finalX-1][finalY].setText(clickedButton.getText());
+                    buttons[finalX][finalY].setText("");
+                }
+            }
+            // clickedButton is somewhere in the middle
+            else {
+                // button above clickedButton
+                if (Objects.equals(buttons[finalX][finalY - 1].getText(), "")) {
+                    buttons[finalX][finalY - 1].setText(clickedButton.getText());
+                    buttons[finalX][finalY].setText("");
+                }
+                // button below clickedButton
+                if (Objects.equals(buttons[finalX][finalY + 1].getText(), "")) {
+                    buttons[finalX][finalY + 1].setText(clickedButton.getText());
+                    buttons[finalX][finalY].setText("");
+                }
+                // button to the left of clickedButton
+                if (Objects.equals(buttons[finalX-1][finalY].getText(), "")) {
+                    buttons[finalX-1][finalY].setText(clickedButton.getText());
+                    buttons[finalX][finalY].setText("");
+                }
+                // button to the right of clickedButton
+                if (Objects.equals(buttons[finalX + 1][finalY].getText(), "")) {
+                    buttons[finalX + 1][finalY].setText(clickedButton.getText());
+                    buttons[finalX][finalY].setText("");
+                }
+            }
+        }
+    }
+
+    public boolean checkIfWon() {
+        boolean won = true;
+        int number = 1;
+        for (int x = 0; x < gameWidth; x++) {
+            for (int y = 0; y < gameHeight; y++) {
+                if (!Objects.equals(buttons[x][y].getText(), String.valueOf(number)) && number != 9) {
+                    won = false;
+                }
+                number++;
+            }
+        }
+        return won;
+    }
+
+    public void gameWin(Graphics g) {
+
+        g.setColor(new Color(30, 200, 100));
+        g.setFont((new Font("Ink Free", Font.BOLD, 75)));
+        FontMetrics fm1 = getFontMetrics(g.getFont());
+        g.drawString("You Won!", (SCREEN_WIDTH - fm1.stringWidth("Difficulty"))/2, g.getFont().getSize() + (SCREEN_HEIGHT/7)*2);
+
+        if (!winScreenCreated) {
+
+            restartButton = new JButton("Restart");
+            restartButton.setBounds(SCREEN_WIDTH/2 - (BUTTON_WIDTH/2), (SCREEN_HEIGHT/4)*2, BUTTON_WIDTH, BUTTON_HEIGHT);
+            restartButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    stage = 1;
+                }
+            });
+            this.add(restartButton);
+        }
+    }
+
+    public void removeGameButtons() {
+
+        for (int x = 0; x < gameWidth; x++) {
+            for (int y = 0; y < gameHeight; y++) {
+                this.remove(buttons[x][y]);
+            }
+        }
+        this.revalidate();
+        this.repaint();
+    }
+
+    public void removeRestartButton() {
+        this.remove(restartButton);
+        this.revalidate();
+        this.repaint();
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
     }
+
+
 }
